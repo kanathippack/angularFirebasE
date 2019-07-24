3954f0eec5b3aeee0b90cfbb0518c0ff28932ae2
kanathippack <pack_2541@hotmail.com>
2019-07-24 14:54:06 +0700

    Upload Project v.1 - close# 1

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,97 @@
+import { Injectable } from "@angular/core";
+//import { }'rxjs/add/operator/toPromise';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class AuthService {
+  authState: any = null;
+  constructor(
+   public afAuth: AngularFireAuth
+ ){}
+ 
+ get  authenticated(): boolean {
+  return this.authState !== null;
+}
+  doFacebookLogin(){
+    return new Promise<any>((resolve, reject) => {
+      let provider = new firebase.auth.FacebookAuthProvider();
+      this.afAuth.auth
+      .signInWithPopup(provider)
+      .then(res => {
+        resolve(res);
+        this.authState = 'yes'
+      }, err => {
+        console.log(err);
+        reject(err);
+      })
+    })
+  }
+
+  doTwitterLogin(){
+    return new Promise<any>((resolve, reject) => {
+      let provider = new firebase.auth.TwitterAuthProvider();
+      this.afAuth.auth
+      .signInWithPopup(provider)
+      .then(res => {
+        resolve(res);
+        this.authState = 'yes'
+      }, err => {
+        console.log(err);
+        reject(err);
+      })
+    })
+  }
+
+  doGoogleLogin(){
+    return new Promise<any>((resolve, reject) => {
+      let provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('profile');
+      provider.addScope('email');
+      this.afAuth.auth
+      .signInWithPopup(provider)
+      .then(res => {
+        resolve(res);
+        this.authState = 'yes'
+      }, err => {
+        console.log(err);
+        reject(err);
+      })
+    })
+  }
+
+  doRegister(value){
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
+      .then(res => {
+        resolve(res);
+        this.authState = 'yes'
+      }, err => reject(err))
+    })
+  }
+
+  doLogin(value){
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth().signInWithEmailAndPassword(value.email, value.password)
+      .then(res => {
+        resolve(res);
+        this.authState = 'yes'
+      }, err => reject(err))
+    })
+  }
+
+  doLogout(){
+    return new Promise((resolve, reject) => {
+      if(firebase.auth().currentUser){
+        this.afAuth.auth.signOut();
+        this.authState = null
+        resolve();
+      }
+      else{
+        reject();
+      }
+    });
+  }
+
+
+}
